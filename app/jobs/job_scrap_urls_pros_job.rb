f3428ba4001b1e30ebf6335d9ad79d5b6ef466d8
Souyahibou <souaybou.k@live.fr>
2018-01-17 11:53:23 +0100

    add job for test on heroku 2

--- app/jobs/job_scrap_urls_pros_job.rb
+++ app/jobs/job_scrap_urls_pros_job.rb
@@ -2,7 +2,7 @@ class JobScrapUrlsProsJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    # JobScrapUrlsProsJob.set(wait: 3.minute).perform_later
+    JobScrapUrlsProsJob.set(wait: 1.minute).perform_later
     p "Hello It is" + Time.now.to_s
   end
 end
