c6397e32821e21d7e48ee0b714ca6fa3dbe4f7f3
Souyahibou <souaybou.k@live.fr>
2018-02-24 05:02:18 +0100

    purge2 and refactoring

--- app/services/scrap_fb_pros.rb
+++ app/services/scrap_fb_pros.rb
@@ -2,9 +2,6 @@ require 'pry'
 require 'scrap_urls_pros'  #load 'filename.rb' #every call require/require_relative 'filename' #only once call
 
 class ScrapFbPros
-      # require 'open-uri'
-      # require 'phantomjs'
-      # save_from_on_GoogleDrive(tab);
       # https://www.facebook.com/events/
 
       attr_accessor :access_token
@@ -19,6 +16,21 @@ class ScrapFbPros
         @tab << @hash_head
 
         Evenement.where(:origin_base => 'Facebook').destroy_all
+
+        @fields_of_events             = "events{photos{images},description,event_times,id,name,place,start_time,end_time}"
+
+        @database_of_events           = Evenement
+
+        @champs_exclus_in_compare     = "id","created_at","updated_at","change","origin_base","last_date"
+
+        @msg_event_changed            = "has changed"
+        @msg_event_unchanged          = "no change"
+        @msg_event_new_in_fb          = "nouveau"
+        @msg_event_deleted_in_fb      = "supprimé"
+        @msg_event_undeleted_in_fb    = "Toujours Disponible"
+
+        @data_origin_base_in_bilobaba = "Bilobaba"
+        @data_origin_base_in_facebook = "Facebook"
       end
 
 
@@ -31,7 +43,7 @@ class ScrapFbPros
           session = ScrapUrlsPros.new.set_google_drive_session
           ws = session.spreadsheet_by_key(ENV["SPEADSHEET_LIENS_ET_IDS"]).worksheets[1]   #cle a changer en fonction du lien url du fichier google drive
           tab_of_urls = []
-            for i in 0...(ws.rows.count)    # 1 si en-tête
+            for i in 0...(ws.rows.count)    # 1 si en-tête est ajouté
                 tab_of_urls << ws.rows[i][0];
             end
           tab_of_urls
@@ -40,44 +52,39 @@ class ScrapFbPros
 
 
       def compare_datas_in_database(database)
-          bilobaba_events = database.where(:origin_base=>"Bilobaba")
-          facebook_events = database.where(:origin_base=>"Facebook")
+          bilobaba_events = database.where(:origin_base=>@data_origin_base_in_bilobaba)
+          facebook_events = database.where(:origin_base=>@data_origin_base_in_facebook)
 
           facebook_events.each do |fb_event|
             	bb_event = bilobaba_events.find_by(:event_id=> fb_event.event_id)
-            	if bb_event.nil?  then fb_event.change ="nouveau" else
-            		fb_event.change =  (fb_event.attributes.except("id","created_at","updated_at","change","origin_base","last_date") == bb_event.attributes.except("id","created_at","updated_at","change","origin_base","last_date"))? "no change" : "has changed"
+            	if bb_event.nil?  then fb_event.change = @msg_event_new_in_fb else
+            		fb_event.change =  (fb_event.attributes.except(@champs_exclus_in_compare) == bb_event.attributes.except(@champs_exclus_in_compare))? @msg_event_unchanged : @msg_event_changed
             	end
               fb_event.save
           end
 
           bilobaba_events.each do |bb_event|
             	fb_event = facebook_events.find_by(:event_id=> bb_event.event_id)
-            	if fb_event.nil?  then bb_event.change ="supprimé" else bb_event.change ="Toujours Disponible"  end
+            	if fb_event.nil?  then bb_event.change =@msg_event_deleted_in_fb else bb_event.change = @msg_event_undeleted_in_fb  end
               bb_event.save
           end
       end
 
 
 
-      def scrap_events_facebook_groups(groups)
+      def scrap_events_facebook_groups(groups, database)
           # groups = ScrapFbPros.new.get_all_facebook_groups
             @graph = Koala::Facebook::API.new(ENV["token"])
-            # @graph.get_object("#{group}?fields=events{photos{images}}")
-            # client = FBGraph::Client.new(:client_id => ENV["app_id"],:secret_id => Figaro.env.secret_id ,:token => ENV["token"])
-            # group_page = client.selection.page(group);
-            # data_scrapping = group_page.fields(:events).info!
 
-            data_scrapping = @graph.get_objects(groups, :fields=> "events{photos{images},description,event_times,id,name,place,start_time,end_time}")
+            data_scrapping = @graph.get_objects(groups, :fields=> @fields_of_events)
             data_scrapping.each_key do |group|
                 data_scrapping[group.to_s][:events.to_s][:data.to_s].each do |event|
-                    event_element = Evenement.new
+                    event_element = database.new
                     if event == nil then break else
 
                         event_element.last_date         = Time.now
                         event_element.groupe_id         = group
-                        event_element.origin_base       = "Facebook"
-                        # tabh[:joker]                  =
+                        event_element.origin_base       = @data_origin_base_in_facebook
 
                         event_element.event_id          = event[:id.to_s]
                         event_element.event_name        = event[:name.to_s]
@@ -113,14 +120,14 @@ class ScrapFbPros
                                event_element.event_id                = event_time[:id.to_s]
                                event_element.event_start_time        = event_time[:start_time.to_s]
                                event_element.event_end_time          = event_time[:end_time.to_s]
-                               event_element.event_event_times_data  = ""
+                               event_element.event_event_times_data  = event[:event_times.to_s]
                                event_element.save                                                             #sauvegarde chaque occurence avec les occurences de l'event principal
                            end
                         end
                     end
                 end
             end
-         @tab += ActiveRecord::Base.connection.exec_query("SELECT evenements.* FROM evenements").to_hash
+         @tab += ActiveRecord::Base.connection.exec_query("SELECT #{database.table_name}.* FROM #{database.table_name}").to_hash
       end
 
 
@@ -128,9 +135,9 @@ class ScrapFbPros
 
       def perform
           groups = get_all_facebook_groups                                        #get all group ids
-          scrap_events_facebook_groups(groups)                                    #scrap events
-          compare_datas_in_database(Evenement)
-          @tab
+          scrap_events_facebook_groups(groups, @database_of_events)               #scrap events
+          compare_datas_in_database(@database_of_events)                          #edit/update the events status
+          @tab                                                                    #allow to print the database
       end
 
 private
